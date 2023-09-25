cf7a4d8649398ab4cb9b14e8251295a1b50bf115
Abdul Arif <arif.abdul0002@gmail.com>
2023-09-25 20:33:24 +0700

    add auth feature(login, register and logout)

--- resources/views/components/error-footer.blade.php
+++ resources/views/components/error-footer.blade.php
@@ -1,3 +1,3 @@
 <div class="simple-footer mt-5">
-    Copyright &copy; Stisla 2018
+    Copyright &copy; Abdul Arif With Jago Flutter 2023
 </div>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -33,7 +33,7 @@
                         class="search-close"><i class="fas fa-times"></i></a>
                 </div>
                 <div class="search-item">
-                    <a href="#">#Stisla</a>
+                    <a href="#">SIAKAD</a>
                     <a href="#"
                         class="search-close"><i class="fas fa-times"></i></a>
                 </div>
@@ -245,7 +245,7 @@
                 <img alt="image"
                     src="{{ asset('img/avatar/avatar-1.png') }}"
                     class="rounded-circle mr-1">
-                <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div>
+                <div class="d-sm-none d-lg-inline-block">Hi, {{ auth()->user()->name }}</div>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
                 <div class="dropdown-title">Logged in 5 min ago</div>
@@ -263,9 +263,13 @@
                 </a>
                 <div class="dropdown-divider"></div>
                 <a href="#"
-                    class="dropdown-item has-icon text-danger">
+                    class="dropdown-item has-icon text-danger"
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit()">
                     <i class="fas fa-sign-out-alt"></i> Logout
                 </a>
+                <form id='logout-form' action="{{ route('logout') }}" method="POST" >
+                    @csrf
+                </form>
             </div>
         </li>
     </ul>
